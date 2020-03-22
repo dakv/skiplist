3b898fe2bbe6481a5229483b4092e81ac1590aca
da2018 <da2018t@gmail.com>
2020-03-22 23:04:54 +0800

    INIT

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![feature(box_syntax)]
+#![feature(box_into_raw_non_null)]
+
+mod skipnode;
+pub mod skiplist;
+
+pub const K_MAX_HEIGHT: usize = 12;
---
+++ src/skipnode.rs
@@ -0,0 +1,97 @@
+use std::ptr::NonNull;
+use std::fmt;
+use std::fmt::{Formatter, Error};
+use crate::K_MAX_HEIGHT;
+
+
+/// Key and value should never be None, except the head node.
+/// Forward can not be None, except head node
+#[derive(Copy, Clone, Debug)]
+pub struct Node<T> {
+    pub data: Option<T>,
+    pub forward: [Option<NonNull<Node<T>>>; K_MAX_HEIGHT],
+}
+
+impl<T> Node<T> {
+    pub fn new(data: T) -> Self {
+        Node {
+            data: Some(data),
+            forward: [None; K_MAX_HEIGHT],
+        }
+    }
+
+    pub fn head() -> Self {
+        Node {
+            data: None,
+            forward: [None; K_MAX_HEIGHT],
+        }
+    }
+
+    #[inline]
+    pub fn set_next(&mut self, n: usize, node: Option<NonNull<Node<T>>>) {
+        self.forward[n] = node;
+    }
+
+    #[inline]
+    pub fn get_next(&self, n: usize) -> Option<*mut Node<T>> {
+        assert!(n >= 0);
+        self.forward[n].map(|v| {
+            v.as_ptr()
+        })
+    }
+
+    #[inline]
+    pub fn get_mut_next(&self, n: usize) -> Option<NonNull<Node<T>>> {
+        assert!(n >= 0);
+        self.forward[n]
+    }
+}
+
+impl<T> fmt::Display for Node<T>
+    where T: fmt::Display
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        if let Some(ref v) = self.data {
+            write!(f, "{}", v)
+        } else {
+            Ok(())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Node;
+    use std::ptr::NonNull;
+
+    #[test]
+    fn test_new_node() {
+        let node = Node::<u8>::head();
+        assert_eq!(format!("{}", node), "");
+
+        let node = Node::new(1);
+        assert_eq!(format!("{}", node), "1");
+
+        let node = Node::new("da");
+        assert_eq!(format!("{}", node), "da");
+    }
+
+    #[test]
+    fn test_next() {
+        let mut node = Node::new(1);
+        let mut next = Node::new(2);
+        let mut tail = Node::new(3);
+        node.set_next(2, NonNull::new(&mut next));
+        let ret = node.get_next(1);
+        assert!(ret.is_none());
+        let ret = node.get_next(2);
+        assert!(ret.is_some());
+        next.set_next(3, NonNull::new(&mut tail));
+        unsafe {
+            if let Some(v) = next.get_next(3) {
+                let d = *v;
+                assert_eq!(d.data.unwrap(), 3);
+            }
+        }
+    }
+}
