a1f4eabe59ff3bcad190a15c513386a90f838c11
dkkb <dabkb@aol.com>
2021-07-15 19:20:05 +0800

    add simplify comparator function

--- src/cmp.rs
+++ src/cmp.rs
@@ -2,6 +2,36 @@ use std::cmp::Ordering;
 
 pub trait BaseComparator {
     fn compare(&self, a: &[u8], b: &[u8]) -> Ordering;
+
+    /// Less than
+    fn lt(&self, a: &[u8], b: &[u8]) -> bool {
+        self.compare(a, b) == Ordering::Less
+    }
+
+    /// Less than or equal
+    fn le(&self, a: &[u8], b: &[u8]) -> bool {
+        self.compare(a, b) != Ordering::Greater
+    }
+
+    /// Greater than
+    fn gt(&self, a: &[u8], b: &[u8]) -> bool {
+        self.compare(a, b) == Ordering::Greater
+    }
+
+    /// Greater than or equal
+    fn ge(&self, a: &[u8], b: &[u8]) -> bool {
+        self.compare(a, b) != Ordering::Less
+    }
+
+    /// Equal
+    fn eq(&self, a: &[u8], b: &[u8]) -> bool {
+        self.compare(a, b) == Ordering::Equal
+    }
+
+    /// Not equal
+    fn ne(&self, a: &[u8], b: &[u8]) -> bool {
+        self.compare(a, b) != Ordering::Equal
+    }
 }
 
 #[derive(Default)]
@@ -31,5 +61,16 @@ mod tests {
         assert_eq!(cmp.compare(&[1], &[2]), Ordering::Less);
         assert_eq!(cmp.compare(&[2], &[2]), Ordering::Equal);
         assert_eq!(cmp.compare(&[2], &[1]), Ordering::Greater);
+    }
+
+    #[test]
+    fn test_simplify() {
+        let cmp = DefaultComparator::default();
+        assert!(cmp.lt(&[1], &[2]));
+        assert!(cmp.le(&[2], &[2]));
+        assert!(cmp.ge(&[2], &[2]));
+        assert!(cmp.eq(&[2], &[2]));
+        assert!(cmp.gt(&[2], &[1]));
+        assert!(cmp.ne(&[2], &[1]));
     }
 }
